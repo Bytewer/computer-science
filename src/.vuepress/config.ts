cfe0762d3077fa18430aabf69ab494a7e63c0f41
Bytewer <dwx_job@outlook.com>
2024-09-03 00:02:24 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,17 +3,17 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/Computer-Science-Learn-Notes/",
+  base: "/computer-science/",
   // 设置正在使用的语言
   lang: "zh-CN",
   // 设置网站标题
   title: "CSLN",
   theme,
   plugins: [
-      searchProPlugin({
-          // 索引全部内容
-          indexContent: true,
-      })
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+    }),
   ],
   // Enable it with pwa
   shouldPrefetch: false,
